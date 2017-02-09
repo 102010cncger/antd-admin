afb45fcb973998e1eab5e2732a0e264b1dba36c9
bo.wang <102010cncger@sina.com>
2017-02-09 17:50:02 +0800

    如何使用state中变量

--- src/models/test.js
+++ src/models/test.js
@@ -1,10 +1,36 @@
+import { parse } from 'qs'
 
 export default {
   namespace: 'test',
   state: {
     name:"11111111111111111"
   },
-  reducers: {},
-  effects: {},
-  subscriptions: {},
+  reducers: {
+    // testReducers (state, {payload}) {
+    //   return { ...state,...payload}
+    // }
+    testReducers (state, {payload:{name}}) {
+      return { ...state,name}
+    }
+  },
+  effects: {
+    *testEffects ({ payload }, { call, put }) {
+      yield put({
+        type: 'testReducers',
+        payload: {
+          name:"2222222222222"
+        }
+      })
+    }
+  },
+  subscriptions: {
+    setup ({ dispatch, history }) {
+      dispatch({
+        type: 'testReducers',
+        payload: {
+          name:"setup-->1111111111111"
+        }
+      })
+    }
+  },
 };
--- src/routes/Test.js
+++ src/routes/Test.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { PropTypes } from 'react'
+import { Button } from 'antd';
 import { connect } from 'dva';
 import styles from './Test.css';
 
 function Test({ location, dispatch, test }) {
-  const { name } = test
+  const { name } = test;
+  function testReducers() {
+    dispatch({
+      type: 'test/testReducers',
+      payload:{
+        name:"323232"
+      }
+    })
+  }
+  function testEffects() {
+    dispatch({
+      type: 'test/testEffects'
+    })
+  }
   return (
     <div className={styles.normal}>
       <span>from state: {test.name}</span>
       <br></br>
       <span>from param: {name}</span>
+      <Button onClick={testReducers}>testReducers</Button>
+      <Button onClick={testEffects}>testEffects</Button>
     </div>
   );
 }
@@ -16,5 +32,11 @@ function Test({ location, dispatch, test }) {
 function mapStateToProps({ test }) {
   return {test};
 }
+
+// Test.propTypes = {
+//   test: PropTypes.object,
+//   testReducers: PropTypes.func,
+//   testEffects: PropTypes.func
+// }
 
 export default connect(mapStateToProps)(Test);
