5380c9651f2a19998db072a0b20bd30b8d514c45
bo.wang <102010cncger@sina.com>
2017-03-09 21:11:59 +0800

    路由问题解决

--- src/services/EitSourceAccount.js
+++ src/services/EitSourceAccount.js
@@ -1,28 +1,28 @@
 import { request } from '../utils'
 
 export async function query (params) {
-  return request('/api/users', {
+  return request('/api-base/findByList', {
     method: 'get',
     data: params
   })
 }
 
 export async function create (params) {
-  return request('/api/users', {
+  return request('/api-base/EitSourceAccountService', {
     method: 'post',
     data: params
   })
 }
 
 export async function remove (params) {
-  return request('/api/users', {
+  return request('/api-base/EitSourceAccountService', {
     method: 'delete',
     data: params
   })
 }
 
 export async function update (params) {
-  return request('/api/users', {
+  return request('/api-base/EitSourceAccountService', {
     method: 'put',
     data: params
   })
